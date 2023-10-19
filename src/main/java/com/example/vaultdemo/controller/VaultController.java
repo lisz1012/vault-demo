27396126536ff786d80cceb33bae31f70c673f03
lisz1012 <lisz1012@163.com>
2023-10-19 00:46:53 -0700

    三个路径全 work

--- src/main/java/com/example/vaultdemo/controller/VaultController.java
+++ src/main/java/com/example/vaultdemo/controller/VaultController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.VaultResponse;
 import org.springframework.vault.support.VaultResponseSupport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,11 +27,16 @@ public class VaultController {
         VaultResponseSupport<MySecretWrapper> response = vaultTemplate.read("secret/data/data/vault-demo/dev", MySecretWrapper.class);
         String username = response.getData().getData().getUsername();
         String password = response.getData().getData().getPassword();
+        String url = response.getData().getData().getUrl();
         System.out.println("Username: " + username);
         System.out.println("Password: " + password);
-        VaultResponseSupport<MyUrl> response2 = vaultTemplate.read("secret/data/vault-demo", MyUrl.class);
-        String url = response2.getData().getUrl();
-        System.out.println("URL: " + url);
+        System.out.println("Url: " + url);
+        VaultResponseSupport<MySecretWrapper> response2 = vaultTemplate.read("secret/data/data/vault-demo", MySecretWrapper.class);
+        String url2 = response2.getData().getData().getUrl();
+        System.out.println("URL2: " + url2);
+        VaultResponseSupport<MySecretWrapper> response3 = vaultTemplate.read("secret/data/vault-demo", MySecretWrapper.class);
+        String url3 = response3.getData().getData().getUrl();
+        System.out.println("URL3: " + url3);
         return password;
     }
 }
